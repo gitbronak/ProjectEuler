bf88a24b0eaeeebb3b3ce7312c72a00e064289d7
Ronak <patelronak_28@yahoo.com>
2015-05-27 14:01:00 -0400

    Problem 3

--- src/Problems/P3.scala
+++ src/Problems/P3.scala
@@ -1,8 +1,22 @@
 package Problems
 
 /**
- * @author ronak
+ * @author Ronak
+ * 
+ * What is the largest prime factor of the number 600851475143 ?
+ * 
  */
 object P3 {
-  
+  def main(args: Array[String]) {
+    var value = 600851475143L;
+    var factor = 2L;
+    
+    while(value > factor) {
+      if(value % factor == 0){
+        value = value/factor;
+      }
+      factor+=1;
+    }
+    println(value);
+  }
 }
