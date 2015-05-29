45edbc06523e1e82458b18078a3feead7b1f3647
Ronak <patelronak_28@yahoo.com>
2015-05-29 12:43:42 -0400

    P4 done!

--- src/Problems/P4.scala
+++ src/Problems/P4.scala
@@ -9,7 +9,62 @@ import scala.util.control._
  */
 object P4 {
   def main(args: Array[String]) {
-    
+    var timess: Long = System.currentTimeMillis;
+    println(solution1());
+    var timese: Long = System.currentTimeMillis;
+    println("Run Time:", timese - timess);
+
+    timess = System.currentTimeMillis;
+    println(solution2());
+    timese = System.currentTimeMillis;
+    println("Run Time:", timese - timess);
+
+  }
+
+  def solution2(): Int = {
+    var value: Int = 999 * 999;
+    var min: Int = 10001;
+
+    while (value > min) {
+      if (isPalindrome(value)) {
+        for (i <- 999 to 99 by -1) {
+          if (value % i == 0) {
+            if (value / i > 99 && value / i < 999) {
+              return value;
+            }
+          }
+        }
+      }
+      value -= 1;
+    }
+
+    return 0;
+  }
+
+  def solution1(): Int = {
+    var x: Int = 999;
+    var limit: Int = 99;
+    var palindromes = List[Int]();
+
+    while (x > limit) {
+
+      val loop = new Breaks;
+      var y: Int = 999;
+
+      loop.breakable {
+        while (y > limit) {
+          var product: Int = x * y;
+          if (isPalindrome(product)) {
+            palindromes = palindromes.++(List[Int](product));
+            limit = y;
+            loop.break();
+          }
+          y -= 1;
+        }
+      }
+      x -= 1;
+    }
+    return palindromes.max;
   }
 
   def isPalindrome(number: Int): Boolean = {
