434fac1ccfb7fe6f2538d6b3a42defcce947e73b
Ronak <patelronak_28@yahoo.com>
2015-06-29 11:03:32 -0400

    P23

---
+++ src/problems/P23.scala
@@ -0,0 +1,54 @@
+package problems
+
+import utils.MathHelper
+import scala.util.control.Breaks._
+import java.util.ArrayList
+import java.util.HashMap
+
+/**
+ * @author Ronak
+ */
+
+object P23 {
+  def main(args: Array[String]) {
+
+    var abnum: ArrayList[Int] = new ArrayList[Int];
+    var sums: HashMap[Int, Boolean] = new HashMap[Int, Boolean];
+
+    for (i <- 12 to 28123) {
+      if (MathHelper.sumOfDivisors(i) > i)
+        abnum.add(i);
+      sums.put(i, false);
+    }
+
+    var total: Long = ((28123 * (28123 + 1) / 2));
+
+    breakable {
+      for (j <- 0 to abnum.size() - 1) {
+        var x = abnum.get(j);
+        var i = j;
+        breakable {
+          while (i < abnum.size() - 1) {
+            var y = abnum.get(i);
+            var z = x + y;
+            if (z > 28123)
+              break;
+            if (!sums.get(z)) {
+              sums.put(z, true);
+            }
+
+            i += 1;
+          }
+
+        }
+      }
+    }
+
+    for (i <- 12 to 28123) {
+      if (sums.get(i))
+        total -= i;
+    }
+
+    println(total);
+  }
+}
